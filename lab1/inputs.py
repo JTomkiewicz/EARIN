988542841cb1748811df6d0e8fe6d32e3b217498
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-03-07 13:22:29 +0100

    read inputs in separate file

---
+++ lab1/inputs.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+
+def getStartPoint(func_type):
+    start_point_type = 'scalar number' if func_type == 0 else 'initial vector'
+
+    # chose starting point type
+    start_method = int(input(
+        'Chose how to define start point\n0 - ' + start_point_type + '\n1 - Generate from uniform distribution\nInput: '))
+
+    while start_method not in [0, 1]:
+        start_method = int(input('Insert 0 or 1! Input: '))
+
+    if start_method == 0:  # inserted by user
+        if func_type == 0:  # scalar number
+            start_p = input('Insert starting point\nInput: ')
+
+            while not np.isscalar(start_p):  # point must be scalar
+                start_p = input('Nr must be scalar! Input: ')
+        else:  # initial vector
+            print('')
+    else:  # generated from uniform distibution
+        start_point_type = start_point_type.replace('initial ', '')
+
+        low_rage = input('Insert low range ' + start_point_type + '\nInput: ')
+        high_rage = input('Insert high range' + start_point_type + '\nInput: ')
+
+        start_p = np.random.uniform(low_rage, high_rage)
+
+    return start_p
+
+
+def getPositiveDefineMatrix():  # input positive define matrix A
+    params = input(
+        'Give params for matrix A. Rows have to be separated by ; (example: 2 -1; -1 2)\nInput: ')
+    # use numpy to create matrix
+    matrix = np.matrix(params)
+
+    # check if matrix is positive-define
+    if not np.all(np.linalg.eigvals(matrix) > 0):
+        params = input('Matrix must be positive-define! Input: ')
+        matrix = np.matrix(params)
+
+    return matrix
+
+
+def getDimentionalVector(d):
+    # d-dimensional vector full of 0s
+    array = np.zeros((d, d))
+
+    # add 1s
+    for x in range(1, d):
+        array[x-1][x] = 1
+
+    return array
+
+
+def readScalar(letter):
+    number = input('Insert parameter ' + letter + '\nInput: ')
+
+    while not np.isscalar(number):  # abcd must be scalar nrs
+        number = input('Nr must be scalar! Input: ')
+
+    return number
+
+
+def getParams(func_type):
+    if(func_type == 0):  # Fx
+        a = readScalar('a')
+        b = readScalar('b')
+        c = readScalar('c')
+        d = readScalar('d')
+
+        return a, b, c, d
+
+    # Gx
+    c = input('Insert parameter c\nInput: ')
+
+    while not np.isscalar(c):  # c must be scalar nr
+        c = input('Nr must be scalar! Input: ')
+
+    vector_dim = int(input('Insert vector d dimension\nInput: '))
+
+    while not vector_dim > 0:
+        vector_dim = int(input('Vector dimension must be > 0\nInput: '))
+
+    return c, vector_dim
--- lab1/main.py
+++ lab1/main.py
@@ -1,89 +1,11 @@
 import numpy as np
 
+# import inputs validator
+import inputs
+
 # import functions from other files
 import newtons
 import gradient
-
-
-def getStartPoint(func_type):
-    start_point_type = 'scalar number' if func_type == 0 else 'initial vector'
-
-    # chose starting point type
-    start_method = int(input(
-        'Chose how to define start point\n0 - ' + start_point_type + '\n1 - Generate from uniform distribution\nInput: '))
-
-    while start_method not in [0, 1]:
-        start_method = int(input('Insert 0 or 1! Input: '))
-
-    if start_method == 0:  # inserted by user
-        if func_type == 0:  # scalar number
-            start_p = input('Insert starting point\nInput: ')
-
-            while not np.isscalar(start_p):  # point must be scalar
-                start_p = input('Nr must be scalar! Input: ')
-        else:  # initial vector
-            print('')
-    else:  # generated from uniform distibution
-        start_point_type = start_point_type.replace('initial ', '')
-
-        low_rage = input('Insert low range ' + start_point_type + '\nInput: ')
-        high_rage = input('Insert high range' + start_point_type + '\nInput: ')
-
-        start_p = np.random.uniform(low_rage, high_rage)
-
-    return start_p
-
-
-def getPositiveDefineMatrix():  # input positive define matrix A
-    params = input(
-        'Give params for matrix A. Rows have to be separated by ;\nInput: ')
-    # use numpy to create matrix
-    matrix = np.matrix(params)
-
-    # check if matrix is positive-define
-    if not np.all(np.linalg.eigvals(matrix) > 0):
-        params = input('Matrix must be positive-define! Input: ')
-        matrix = np.matrix(params)
-
-    return matrix
-
-
-def getDimentionalVector(d):
-    # d-dimensional vector full of 0s
-    array = np.zeros((d, d))
-
-    # add 1s
-    for x in range(1, d):
-        array[x-1][x] = 1
-
-    return array
-
-
-def getParams(func_type):
-    # empty dictionary
-    p = dict()
-
-    # func F(x)
-    if(func_type == 0):
-        for letter in ['a', 'b', 'c', 'd']:
-            p[letter] = input('Insert parameter ' + letter + '\nInput: ')
-
-            while not np.isscalar(p[letter]):  # abcd must be scalar nrs
-                p[letter] = input('Nr must be scalar! Input: ')
-    else:  # func G(x)
-        p['c'] = input('Insert parameter c\nInput: ')
-
-        while not np.isscalar(p['c']):  # c must be scalar nr
-            p['c'] = input('Nr must be scalar! Input: ')
-
-        p['vectorDimension'] = int(input('Insert vector d dimension\nInput: '))
-
-        while not p['vectorDimension'] > 0:
-            p['vectorDimension'] = int(
-                input('Vector dimension must be > 0\nInput: '))
-
-    # return dictionary
-    return p
 
 
 def main():
@@ -101,30 +23,32 @@ def main():
     while chosen_func_type not in [0, 1]:
         chosen_func_type = int(input('Insert 0 or 1! Input: '))
 
-    # get scalar parameters
-    params = getParams(chosen_func_type)
+    # read and validate inputs
+    params = inputs.getParams(chosen_func_type)
 
-    # when G(x) then create vector b and matrix A
-    if(chosen_func_type == 1):
-        params['b'] = getDimentionalVector(params['vectorDimension'])
-        params['A'] = getPositiveDefineMatrix()
+    if(chosen_func_type == 0):  # Fx
+        a = params[0]
+        b = params[1]
+        c = params[2]
+        d = params[3]
+    else:  # Gx
+        c = params[0]
+        b = inputs.getDimentionalVector(params[1])
+        A = inputs.getPositiveDefineMatrix()
 
-    starting_point = float(getStartPoint(chosen_func_type))
+    # read and validate input start point
+    start_point = inputs.getStartPoint(chosen_func_type)
 
     if chosen_method == 0:  # gradient
         if chosen_func_type == 0:  # Fx
-            gradient.gradientDescentFx(float(params['a']), float(params['b']),
-                                       float(params['c']), float(params['d']), starting_point)
+            gradient.gradientDescentFx(a, b, c, d, start_point)
         else:  # Gx
-            gradient.gradientDescentGx(params['A'], params['b'],
-                                       float(params['c']), float(starting_point))
+            gradient.gradientDescentGx(A, b, c, start_point)
     else:  # newtons
         if chosen_func_type == 0:  # Fx
-            newtons.newtonsFx(float(params['a']), float(params['b']),
-                              float(params['c']), float(params['d']), starting_point)
+            newtons.newtonsFx(a, b, c, d, start_point)
         else:  # Gx
-            newtons.newtonsGx(params['A'], params['b'],
-                              float(params['c']), float(starting_point))
+            newtons.newtonsGx(A, b, c, start_point)
 
 
 if __name__ == "__main__":
