8d6161dc81fe1d0d7262b42408396ac62f8e52ea
Konrad Piotrowski <konrad.piotrowski@oxla.com>
2022-03-17 21:54:45 +0100

    fixed matrix calculations

--- lab2/genetic.py
+++ lab2/genetic.py
@@ -16,13 +16,12 @@ def fitness(x, A, b, c):
 
 
 def geneticAlgorithm(A, b, c):
-    solutions = np.array()
+    solutions = []
 
-    for s in range(10000):
-        solutions = np.append(
-            solutions, [[random.uniform(-1000, 1000)],  [random.uniform(-1000, 1000)]])
+    for s in range(1000):
+        solutions.append(np.matrix([[random.uniform(-1000, 1000)],  [random.uniform(-1000, 1000)]]))
 
-    for i in range(10000):
+    for i in range(1000):
         rankedsolutions = []
 
         for s in solutions:
@@ -41,22 +40,23 @@ def geneticAlgorithm(A, b, c):
             elements.append(s[1])
 
         new_gen = []
-        print(elements[:10])
+
+
 
         for _ in range(10000):
-            e1 = random.choice(elements)[0] * random.uniform(0.99, 1.01)
-            e2 = random.choice(elements)[1] * random.uniform(0.99, 1.01)
+            e1 = random.choice(elements).item(0) * random.uniform(0.99, 1.01)
+            e2 = random.choice(elements).item(1) * random.uniform(0.99, 1.01)
 
-            new_gen.append([[e1], [e2]])
+            new_gen.append(np.matrix([[e1], [e2]]))
 
         solutions = new_gen
 
 
-# c = 5
-# b = np.array([[-11], [1]])
-# A = np.array([[21, -1], [-6, 2]])
+c = 5
+b = np.array([[-11], [1]])
+A = np.array([[21, -1], [-6, 2]])
 # x = np.array([[10], [4]])
 
 # print(fx(x, A, b, c).item(0))
 
-# geneticAlgorithm(A, b, c)
+geneticAlgorithm(A, b, c)
