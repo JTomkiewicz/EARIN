41a443022705cb1b19f078f9693be297b0cb9d1a
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-03-17 21:40:30 +0100

    attempt for quadratic func

--- lab2/genetic.py
+++ lab2/genetic.py
@@ -1,12 +1,13 @@
 import random
+import numpy as np
 
 
-def foo(x, y, z):
-    return 6*x**3 + 9*y**2 + 90*z - 25
+def fx(x, A, b, c):  # fx = x^T*A*x + b^T * x + c
+    return (np.dot(np.dot(np.transpose(x), A), x) + np.dot(np.transpose(b), x) + c).item(0)
 
 
-def fitness(x, y, z):
-    ans = foo(x, y, z)
+def fitness(x, A, b, c):
+    ans = fx(x, A, b, c)
 
     if ans == 0:
         return 99999
@@ -14,36 +15,48 @@ def fitness(x, y, z):
         return abs(1/ans)
 
 
-solutions = []
-for s in range(10000):
-    solutions.append((random.uniform(0, 10000), random.uniform(
+def geneticAlgorithm(A, b, c):
+    solutions = np.array()
 
-        0, 10000), random.uniform(0, 10000)))
+    for s in range(10000):
+        solutions = np.append(
+            solutions, [[random.uniform(-1000, 1000)],  [random.uniform(-1000, 1000)]])
 
-for i in range(10000):
-    rankedsolutions = []
-    for s in solutions:
-        rankedsolutions.append((fitness(s[0], s[1], s[2]), s))
-    rankedsolutions.sort()
-    rankedsolutions.reverse()
-    print(f"=== Gen {i} best solutions ===")
-    print(rankedsolutions[0])
+    for i in range(10000):
+        rankedsolutions = []
 
-    bestsolutions = rankedsolutions[:100]
+        for s in solutions:
+            rankedsolutions.append((fitness(s, A, b, c), s))
 
-    elements = []
-    for s in bestsolutions:
-        elements.append(s[1][0])
-        elements.append(s[1][1])
-        elements.append(s[1][2])
+        rankedsolutions.sort()
+        rankedsolutions.reverse()
 
-    new_gen = []
+        print(f"=== Gen {i} best solutions ===")
+        print(rankedsolutions[0])
 
-    for _ in range(1000):
-        e1 = random.choice(elements) * random.uniform(0.99, 1.01)
-        e2 = random.choice(elements) * random.uniform(0.99, 1.01)
-        e3 = random.choice(elements) * random.uniform(0.99, 1.01)
+        bestsolutions = rankedsolutions[:100]
 
-        new_gen.append((e1, e2, e3))
+        elements = []
+        for s in bestsolutions:
+            elements.append(s[1])
 
-    solutions = new_gen
+        new_gen = []
+        print(elements[:10])
+
+        for _ in range(10000):
+            e1 = random.choice(elements)[0] * random.uniform(0.99, 1.01)
+            e2 = random.choice(elements)[1] * random.uniform(0.99, 1.01)
+
+            new_gen.append([[e1], [e2]])
+
+        solutions = new_gen
+
+
+# c = 5
+# b = np.array([[-11], [1]])
+# A = np.array([[21, -1], [-6, 2]])
+# x = np.array([[10], [4]])
+
+# print(fx(x, A, b, c).item(0))
+
+# geneticAlgorithm(A, b, c)
