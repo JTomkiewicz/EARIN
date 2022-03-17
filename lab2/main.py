4bdfbc20a005c0ecc1abe92a8345c849a29255d6
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-03-17 20:56:24 +0100

    1st version of genetic algo
    Co-authored-by: Piotrowski Konrad 2 (STUD) <konrad.piotrowski2.stud@pw.edu.pl>

--- lab2/main.py
+++ lab2/main.py
@@ -1,9 +1,13 @@
 # import inputs validator
 import inputs
 
+import numpy as np
+
 
 def main():
     c, b, A = inputs.getParams()
+
+    # Gx = np.dot(np.dot(np.transpose(x), A), x) + np.dot(np.transpose(b), x) + c
 
 
 if __name__ == "__main__":
