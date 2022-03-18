3ca4998a7468ce3e9307446428dd5aecf9c95794
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-03-18 22:21:20 +0100

    add range d

--- lab2/genetic.py
+++ lab2/genetic.py
@@ -6,11 +6,11 @@ def fx(x, A, b, c):
     return (np.dot(np.dot(np.transpose(x), A), x) + np.dot(np.transpose(b), x) + c).item(0)
 
 
-def fitness(g1, g2, A, b, c):
+def fitness(g1, g2, A, b, c, d):
     num1 = int(genomeString(g1), 2)
     num2 = int(genomeString(g2), 2)
 
-    if num1 > 1000 or num2 > 1000 or num1 < -1000 or num2 < -1000:
+    if num1 >= 2**d or num2 >= 2**d or num1 < -2**d or num2 < -2**d:
         return -1000
 
     x = [num1, num2]
@@ -36,9 +36,9 @@ def crossover(g1, g2):  # single point crossover
     return [cross1, cross2]
 
 
-def selection(population, A, b, c):
+def selection(population, A, b, c, d):
     # list of 2 randomly chosen elements of population
-    return random.choices(population=population, weights=[fitness(g[0], g[1], A, b, c) for g in population], k=2)
+    return random.choices(population=population, weights=[fitness(g[0], g[1], A, b, c, d) for g in population], k=2)
 
 
 def mutation(g, probability):
@@ -68,12 +68,12 @@ def geneticAlgorithm(popSize, crossProb, mutatProb, n, A, b, c, d):
 
     for i in range(n):
         population = sorted(
-            population, key=lambda genome: fitness(genome[0], genome[1], A, b, c), reverse=True)
+            population, key=lambda genome: fitness(genome[0], genome[1], A, b, c, d), reverse=True)
 
         newGeneration = population[:2]
 
         for _ in range(int(len(population) / 2) - 1):
-            parents = selection(population, A, b, c)
+            parents = selection(population, A, b, c, d)
 
             firstCrossover = crossover(parents[0][0], parents[1][0])
             secondCrossover = crossover(parents[0][1], parents[1][1])
@@ -90,7 +90,7 @@ def geneticAlgorithm(popSize, crossProb, mutatProb, n, A, b, c, d):
         population = newGeneration
 
         population = sorted(
-            population, key=lambda genome: fitness(genome[0], genome[1], A, b, c), reverse=True)
+            population, key=lambda genome: fitness(genome[0], genome[1], A, b, c, d), reverse=True)
 
         # print(f'iteration: {i}')
         # print(f'f(x): {fitness(population[0][0], population[0][1], A, b, c)}')
@@ -98,4 +98,4 @@ def geneticAlgorithm(popSize, crossProb, mutatProb, n, A, b, c, d):
     print(f'Nr of iterations: {i}')
     print(f'x0: {int(genomeString(population[0][0]), 2)}')
     print(f'x1: {int(genomeString(population[0][1]), 2)}')
-    print(f'f(x): {fitness(population[0][0], population[0][1], A, b, c)}')
+    print(f'f(x): {fitness(population[0][0], population[0][1], A, b, c, d)}')
