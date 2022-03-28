005ceb0d58b9bb08c4e8db4014c219e7273c2f23
Konrad Piotrowski <konrad.piotrowski@oxla.com>
2022-03-28 09:02:57 +0200

    tictactoe code refactor

--- lab3/tictactoe.py
+++ lab3/tictactoe.py
@@ -1,174 +1,168 @@
 import time
 
-class Game:
-    def __init__(self):
-        self.initialize_game()
 
-    def initialize_game(self):
-        self.current_state = [['.','.','.'],
-                              ['.','.','.'],
-                              ['.','.','.']]
+def clearBoard():
+    global board
+    board = [['.', '.', '.'],
+             ['.', '.', '.'],
+             ['.', '.', '.']]
 
-        # Player X always plays first
-        self.player_turn = 'X'
 
-    def draw_board(self):
-        for i in range(0, 3):
-            for j in range(0, 3):
-                print('{}|'.format(self.current_state[i][j]), end=" ")
-            print()
+def printBoard():
+    for i in range(0, 3):
+        for j in range(0, 3):
+            print('{}|'.format(board[i][j]), end=" ")
         print()
+    print()
 
-    def is_valid(self, px, py):
-        if px < 0 or px > 2 or py < 0 or py > 2:
-            return False
-        elif self.current_state[px][py] != '.':
-            return False
+
+def isValid(position):
+    if position[0] < 0 or position[0] > 2 or position[1] < 0 or position[1] > 2:
+        return False
+    elif board[position[0]][position[1]] != '.':
+        return False
+    else:
+        return True
+
+
+def isEnd():
+    # Vertical win
+    for i in range(0, 3):
+        if (board[0][i] != '.' and
+            board[0][i] == board[1][i] and
+                board[1][i] == board[2][i]):
+            return board[0][i]
+    # Horizontal win
+    for i in range(0, 3):
+        if (board[i] == ['X', 'X', 'X']):
+            return 'X'
+        elif (board[i] == ['O', 'O', 'O']):
+            return 'O'
+    # Main diagonal win
+    if (board[0][0] != '.' and
+        board[0][0] == board[1][1] and
+            board[0][0] == board[2][2]):
+        return board[0][0]
+    # Second diagonal win
+    if (board[0][2] != '.' and
+        board[0][2] == board[1][1] and
+            board[0][2] == board[2][0]):
+        return board[0][2]
+    # Is whole board full?
+    for i in range(0, 3):
+        for j in range(0, 3):
+            # There's an empty field, we continue the game
+            if (board[i][j] == '.'):
+                return None
+    # It's a tie!
+    return '.'
+
+
+def play():
+    global player_turn
+
+    while True:
+        printBoard()
+        result = isEnd()
+
+        if result != None:
+            if result == 'X':
+                print('The winner is X!')
+            elif result == 'O':
+                print('The winner is O!')
+            elif result == '.':
+                print("It's a tie!")
+
+            clearBoard()
+            return
+
+        if player_turn == 'X':
+            while True:
+                start = time.time()
+                position = [None, None]
+                (m, position) = min_alpha_beta(-2, 2)
+                end = time.time()
+                print('Evaluation time: {}s'.format(round(end - start, 7)))
+                print('Recommended move: X = {}, Y = {}'.format(
+                    position[0], position[1]))
+
+                position[0] = int(input('Insert the X coordinate: '))
+                position[1] = int(input('Insert the Y coordinate: '))
+
+                if isValid(position):
+                    board[position[0]][position[1]] = 'X'
+                    player_turn = 'O'
+                    break
+                else:
+                    print('The move is not valid! Try again.')
+
         else:
-            return True
-
-    def is_end(self):
-        # Vertical win
-        for i in range(0, 3):
-            if (self.current_state[0][i] != '.' and
-                self.current_state[0][i] == self.current_state[1][i] and
-                self.current_state[1][i] == self.current_state[2][i]):
-                return self.current_state[0][i]
-
-        # Horizontal win
-        for i in range(0, 3):
-            if (self.current_state[i] == ['X', 'X', 'X']):
-                return 'X'
-            elif (self.current_state[i] == ['O', 'O', 'O']):
-                return 'O'
-
-        # Main diagonal win
-        if (self.current_state[0][0] != '.' and
-            self.current_state[0][0] == self.current_state[1][1] and
-            self.current_state[0][0] == self.current_state[2][2]):
-            return self.current_state[0][0]
-
-        # Second diagonal win
-        if (self.current_state[0][2] != '.' and
-            self.current_state[0][2] == self.current_state[1][1] and
-            self.current_state[0][2] == self.current_state[2][0]):
-            return self.current_state[0][2]
-
-        # Is whole board full?
-        for i in range(0, 3):
-            for j in range(0, 3):
-                # There's an empty field, we continue the game
-                if (self.current_state[i][j] == '.'):
-                    return None
-
-        # It's a tie!
-        return '.'
-
-    def play_alpha_beta(self):
-        while True:
-            self.draw_board()
-            self.result = self.is_end()
-
-            if self.result != None:
-                if self.result == 'X':
-                    print('The winner is X!')
-                elif self.result == 'O':
-                    print('The winner is O!')
-                elif self.result == '.':
-                    print("It's a tie!")
+            (m, position) = max(-2, 2)
+            board[position[0]][position[1]] = 'O'
+            player_turn = 'X'
 
 
-                self.initialize_game()
-                return
+def max(alpha, beta):
+    maxv = -2
+    max_pos = [None, None]
+    result = isEnd()
 
-            if self.player_turn == 'X':
+    if result == 'X':
+        min_pos = [0, 0]
+        return (-1, min_pos)
+    elif result == 'O':
+        min_pos = [0, 0]
+        return (1, min_pos)
+    elif result == '.':
+        min_pos = [0, 0]
+        return (0, min_pos)
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if board[i][j] == '.':
+                board[i][j] = 'O'
+                (m, min_pos) = min_alpha_beta(alpha, beta)
+                if m > maxv:
+                    maxv = m
+                    max_pos = [i, j]
+                board[i][j] = '.'
+                # Next two ifs in Max and Min are the only difference between regular algorithm and minimax
+                if maxv >= beta:
+                    return (maxv, max_pos)
+                if maxv > alpha:
+                    alpha = maxv
+    return (maxv, max_pos)
 
-                while True:
-                    start = time.time()
-                    (m, qx, qy) = self.min_alpha_beta(-2, 2)
-                    end = time.time()
-                    print('Evaluation time: {}s'.format(round(end - start, 7)))
-                    print('Recommended move: X = {}, Y = {}'.format(qx, qy))
 
-                    px = int(input('Insert the X coordinate: '))
-                    py = int(input('Insert the Y coordinate: '))
+def min_alpha_beta(alpha, beta):
+    minv = 2
+    min_pos = [None, None]
+    result = isEnd()
+    if result == 'X':
+        min_pos = [0, 0]
+        return (-1, min_pos)
+    elif result == 'O':
+        min_pos = [0, 0]
+        return (1, min_pos)
+    elif result == '.':
+        min_pos = [0, 0]
+        return (0, min_pos)
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if board[i][j] == '.':
+                board[i][j] = 'X'
+                (m, max_pos) = max(alpha, beta)
+                if m < minv:
+                    minv = m
+                    min_pos = [i, j]
+                board[i][j] = '.'
+                if minv <= alpha:
+                    return (minv, min_pos)
+                if minv < beta:
+                    beta = minv
+    return (minv, min_pos)
 
-                    qx = px
-                    qy = py
 
-                    if self.is_valid(px, py):
-                        self.current_state[px][py] = 'X'
-                        self.player_turn = 'O'
-                        break
-                    else:
-                        print('The move is not valid! Try again.')
-
-            else:
-                (m, px, py) = self.max_alpha_beta(-2, 2)
-                self.current_state[px][py] = 'O'
-                self.player_turn = 'X'
-
-    def max_alpha_beta(self, alpha, beta):
-        maxv = -2
-        px = None
-        py = None
-
-        result = self.is_end()
-
-        if result == 'X':
-            return (-1, 0, 0)
-        elif result == 'O':
-            return (1, 0, 0)
-        elif result == '.':
-            return (0, 0, 0)
-        for i in range(0, 3):
-            for j in range(0, 3):
-                if self.current_state[i][j] == '.':
-                    self.current_state[i][j] = 'O'
-                    (m, min_i, in_j) = self.min_alpha_beta(alpha, beta)
-                    if m > maxv:
-                        maxv = m
-                        px = i
-                        py = j
-                    self.current_state[i][j] = '.'
-                    # Next two ifs in Max and Min are the only difference between regular algorithm and minimax
-                    if maxv >= beta:
-                        return (maxv, px, py)
-                    if maxv > alpha:
-                        alpha = maxv
-        return (maxv, px, py)
-
-    def min_alpha_beta(self, alpha, beta):
-
-        minv = 2
-        qx = None
-        qy = None
-        result = self.is_end()
-        if result == 'X':
-            return (-1, 0, 0)
-        elif result == 'O':
-            return (1, 0, 0)
-        elif result == '.':
-            return (0, 0, 0)
-        for i in range(0, 3):
-            for j in range(0, 3):
-                if self.current_state[i][j] == '.':
-                    self.current_state[i][j] = 'X'
-                    (m, max_i, max_j) = self.max_alpha_beta(alpha, beta)
-                    if m < minv:
-                        minv = m
-                        qx = i
-                        qy = j
-                    self.current_state[i][j] = '.'
-                    if minv <= alpha:
-                        return (minv, qx, qy)
-                    if minv < beta:
-                        beta = minv
-        return (minv, qx, qy)
-
-def main():
-    g = Game()
-    g.play_alpha_beta()
-
-if __name__ == "__main__":
-    main()
+clearBoard()
+global player_turn
+player_turn = 'X'
+play()
