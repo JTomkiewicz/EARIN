1f614da009489e0c88723167709b23cf2c1f979f
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-04-02 16:19:44 +0200

    simplify code

--- lab3/tictactoe.py
+++ lab3/tictactoe.py
@@ -1,4 +1,13 @@
-import time
+def readInt(msg: str):  # read input and check if it is integer
+    while True:
+        try:
+            number = input(f'Insert {msg}: ')
+            number = int(number)
+            break
+        except ValueError:
+            print('Input must be a int!')
+
+    return int(number)
 
 
 def clearBoard():
@@ -9,160 +18,175 @@ def clearBoard():
 
 
 def printBoard():
-    for i in range(0, 3):
-        for j in range(0, 3):
-            print('{}|'.format(board[i][j]), end=" ")
-        print()
+    for i in range(3):
+        print(f'|{board[i][0]}|{board[i][1]}|{board[i][2]}|')
     print()
 
 
 def isValid(position):
-    if position[0] < 0 or position[0] > 2 or position[1] < 0 or position[1] > 2:
+    # check if position is not out of range or position is occupied
+    if position[0] not in range(3):
+        return False
+    elif position[1] not in range(3):
         return False
     elif board[position[0]][position[1]] != '.':
         return False
-    else:
-        return True
+
+    # position is valid
+    return True
 
 
 def isEnd():
-    # Vertical win
-    for i in range(0, 3):
-        if (board[0][i] != '.' and
-            board[0][i] == board[1][i] and
-                board[1][i] == board[2][i]):
+    # vertical win
+    for i in range(3):
+        if board[0][i] != '.' and board[0][i] == board[1][i] == board[2][i]:
             return board[0][i]
-    # Horizontal win
-    for i in range(0, 3):
-        if (board[i] == ['X', 'X', 'X']):
-            return 'X'
-        elif (board[i] == ['O', 'O', 'O']):
-            return 'O'
-    # Main diagonal win
-    if (board[0][0] != '.' and
-        board[0][0] == board[1][1] and
-            board[0][0] == board[2][2]):
+
+    # horizontal win
+    for i in range(3):
+        if board[i][0] != '.' and board[i][0] == board[i][1] == board[i][2]:
+            return board[i][0]
+
+    # first diagonal win
+    if board[0][0] != '.' and board[0][0] == board[1][1] == board[2][2]:
         return board[0][0]
-    # Second diagonal win
-    if (board[0][2] != '.' and
-        board[0][2] == board[1][1] and
-            board[0][2] == board[2][0]):
+
+    # second diagonal win
+    if board[0][2] != '.' and board[0][2] == board[1][1] == board[2][0]:
         return board[0][2]
-    # Is whole board full?
-    for i in range(0, 3):
-        for j in range(0, 3):
-            # There's an empty field, we continue the game
-            if (board[i][j] == '.'):
+
+    # check if board is fully occupied
+    for i in range(3):
+        for j in range(3):
+            # check if there is still empty space
+            if board[i][j] == '.':
                 return None
-    # It's a tie!
+
+    # tie
     return '.'
 
 
 def play():
-    global player_turn
+    global playerTurn
 
     while True:
         printBoard()
         result = isEnd()
 
+        # check if game ended
         if result != None:
             if result == 'X':
-                print('The winner is X!')
+                print('Game over! X win!')
             elif result == 'O':
-                print('The winner is O!')
+                print('Game over! O win!')
             elif result == '.':
-                print("It's a tie!")
+                print("Game over! It is a tie!")
 
             clearBoard()
             return
 
-        if player_turn == 'X':
+        # player turn
+        if playerTurn == 'X':
             while True:
-                start = time.time()
+
                 position = [None, None]
-                (m, position) = min(-2, 2)
-                end = time.time()
-                print('Evaluation time: {}s'.format(round(end - start, 7)))
-                print('Recommended move: X = {}, Y = {}'.format(
-                    position[0], position[1]))
+                position = min(-2, 2)[1]
 
-                position[0] = int(input('Insert the X coordinate: '))
-                position[1] = int(input('Insert the Y coordinate: '))
+                print(
+                    f'Algorithm recomends to place X at row = {position[0]} and column = {position[1]}')
 
+                # get coordinates from user
+                position[0] = readInt('row number')
+                position[1] = readInt('column number')
+
+                # check if input is valid
                 if isValid(position):
                     board[position[0]][position[1]] = 'X'
-                    player_turn = 'O'
+                    playerTurn = 'O'
                     break
                 else:
-                    print('The move is not valid! Try again.')
+                    print('Invalid move! Try again.')
 
         else:
-            (m, position) = max(-2, 2)
+            position = max(-2, 2)[1]
             board[position[0]][position[1]] = 'O'
-            player_turn = 'X'
+            playerTurn = 'X'
 
 
 def max(alpha, beta):
-    maxv = -2
-    max_pos = [None, None]
+    maxValue = -2
+    maxPosition = [None, None]
     result = isEnd()
 
     if result == 'X':
-        min_pos = [0, 0]
-        return (-1, min_pos)
+        return (-1, [0, 0])
     elif result == 'O':
-        min_pos = [0, 0]
-        return (1, min_pos)
+        return (1, [0, 0])
     elif result == '.':
-        min_pos = [0, 0]
-        return (0, min_pos)
-    for i in range(0, 3):
-        for j in range(0, 3):
+        return (0, [0, 0])
+
+    for i in range(3):
+        for j in range(3):
             if board[i][j] == '.':
                 board[i][j] = 'O'
-                (m, min_pos) = min(alpha, beta)
-                if m > maxv:
-                    maxv = m
-                    max_pos = [i, j]
+                minValue = min(alpha, beta)[0]
+
+                if minValue > maxValue:
+                    maxValue = minValue
+                    maxPosition = [i, j]
+
                 board[i][j] = '.'
-                # Next two ifs in Max and Min are the only difference between regular algorithm and minimax
-                if maxv >= beta:
-                    return (maxv, max_pos)
-                if maxv > alpha:
-                    alpha = maxv
-    return (maxv, max_pos)
+
+                # alpha-beta pruning
+                if maxValue >= beta:
+                    return (maxValue, maxPosition)
+                if maxValue > alpha:
+                    alpha = maxValue
+
+    return (maxValue, maxPosition)
 
 
 def min(alpha, beta):
-    minv = 2
-    min_pos = [None, None]
+    minValue = 2
+    minPosition = [None, None]
     result = isEnd()
+
     if result == 'X':
-        min_pos = [0, 0]
-        return (-1, min_pos)
+        return (-1, [0, 0])
     elif result == 'O':
-        min_pos = [0, 0]
-        return (1, min_pos)
+        return (1, [0, 0])
     elif result == '.':
-        min_pos = [0, 0]
-        return (0, min_pos)
-    for i in range(0, 3):
-        for j in range(0, 3):
+        return (0, [0, 0])
+
+    for i in range(3):
+        for j in range(3):
             if board[i][j] == '.':
                 board[i][j] = 'X'
-                (m, max_pos) = max(alpha, beta)
-                if m < minv:
-                    minv = m
-                    min_pos = [i, j]
+                maxValue = max(alpha, beta)[0]
+
+                if maxValue < minValue:
+                    minValue = maxValue
+                    minPosition = [i, j]
+
                 board[i][j] = '.'
-                if minv <= alpha:
-                    return (minv, min_pos)
-                if minv < beta:
-                    beta = minv
-    return (minv, min_pos)
+
+                # alpha-beta pruning
+                if minValue <= alpha:
+                    return (minValue, minPosition)
+                if minValue < beta:
+                    beta = minValue
+
+    return (minValue, minPosition)
 
 
-clearBoard()
-global player_turn
-player_turn = 'X'
-play()
+def main():
+    clearBoard()
+
+    global playerTurn
+    playerTurn = 'X'
+
+    play()
+
+
+if __name__ == "__main__":
+    main()
