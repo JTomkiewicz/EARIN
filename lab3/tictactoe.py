bc32bd4898ea68d585edc0377c74cd2dc9989279
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-04-03 13:35:58 +0200

    add comments & show what func returns

--- lab3/tictactoe.py
+++ lab3/tictactoe.py
@@ -1,4 +1,4 @@
-def readInt(msg: str):
+def readInt(msg: str) -> int:
     while True:
         try:
             number = input(f'Insert {msg}: ')
@@ -10,33 +10,33 @@ def readInt(msg: str):
     return int(number)
 
 
-def clearBoard():
+def clearBoard() -> None:
     global board
     board = [['.', '.', '.'],
              ['.', '.', '.'],
              ['.', '.', '.']]
 
 
-def printBoard():
+def printBoard() -> None:
     for i in range(3):
         print(f'|{board[i][0]}|{board[i][1]}|{board[i][2]}|')
     print()
 
 
-def isValid(position):
-    # check if position is out of range or position is occupied
+def isPositionValid(position: list) -> bool:
+    # check if position is out of range
     if position[0] not in range(3):
         return False
     elif position[1] not in range(3):
         return False
-    elif board[position[0]][position[1]] != '.':
+    elif board[position[0]][position[1]] != '.':  # check if position is not already occupied
         return False
 
-    # position is valid
+    # position is fine
     return True
 
 
-def isEnd():
+def isGameEnded():
     # vertical win
     for i in range(3):
         if board[0][i] != '.' and board[0][i] == board[1][i] == board[2][i]:
@@ -47,39 +47,40 @@ def isEnd():
         if board[i][0] != '.' and board[i][0] == board[i][1] == board[i][2]:
             return board[i][0]
 
-    # first diagonal win
+    # 1st diagonal win
     if board[0][0] != '.' and board[0][0] == board[1][1] == board[2][2]:
         return board[0][0]
 
-    # second diagonal win
+    # 2nd diagonal win
     if board[0][2] != '.' and board[0][2] == board[1][1] == board[2][0]:
         return board[0][2]
 
-    # check if board is fully occupied
     for i in range(3):
         for j in range(3):
             # check if there is still empty space
             if board[i][j] == '.':
                 return None
 
-    # tie
+    # no wins && no empty space => tie
     return '.'
 
 
-def game():
+def playGame() -> None:
     global playerTurn
 
     while True:
         printBoard()
-        result = isEnd()
+        result = isGameEnded()
 
         # check if game ended
         if result != None:
+            # show result
             if result == 'X' or result == 'O':
                 print(f'Game over! {result} win!')
             elif result == '.':
                 print("Game over! It is a tie!")
 
+            # clear board
             clearBoard()
             return
 
@@ -90,38 +91,41 @@ def game():
                 position = [None, None]
                 position = min(-2, 2)[1]
 
+                print('Your turn!')
                 print(
-                    f'Algorithm recomends to place X at row = {position[0]} and column = {position[1]}')
+                    f'Algorithm recomends to place X at row = {position[0] + 1} column = {position[1] + 1}')
 
                 # get coordinates from user
                 position[0] = readInt('row number')
                 position[1] = readInt('column number')
 
+                position[0] -= 1
+                position[1] -= 1
+
                 # check if input is valid
-                if isValid(position):
+                if isPositionValid(position):
                     board[position[0]][position[1]] = 'X'
                     playerTurn = 'O'
                     break
                 else:
                     print('Invalid move! Try again.')
-
-        else:
+        else:  # computer turn
             position = max(-2, 2)[1]
             board[position[0]][position[1]] = 'O'
             playerTurn = 'X'
 
 
-def max(alpha: int, beta: int):
+def max(alpha: int, beta: int) -> tuple:
     maxValue = -2
     maxPosition = [None, None]
-    result = isEnd()
+    result = isGameEnded()
 
     if result == 'X':
-        return (-1, [0, 0])
+        return -1, [0, 0]
     elif result == 'O':
-        return (1, [0, 0])
+        return 1, [0, 0]
     elif result == '.':
-        return (0, [0, 0])
+        return 0, [0, 0]
 
     for i in range(3):
         for j in range(3):
@@ -137,24 +141,24 @@ def max(alpha: int, beta: int):
 
                 # alpha-beta pruning
                 if maxValue >= beta:
-                    return (maxValue, maxPosition)
+                    return maxValue, maxPosition
                 if maxValue > alpha:
                     alpha = maxValue
 
-    return (maxValue, maxPosition)
+    return maxValue, maxPosition
 
 
-def min(alpha: int, beta: int):
+def min(alpha: int, beta: int) -> tuple:
     minValue = 2
     minPosition = [None, None]
-    result = isEnd()
+    result = isGameEnded()
 
     if result == 'X':
-        return (-1, [0, 0])
+        return -1, [0, 0]
     elif result == 'O':
-        return (1, [0, 0])
+        return 1, [0, 0]
     elif result == '.':
-        return (0, [0, 0])
+        return 0, [0, 0]
 
     for i in range(3):
         for j in range(3):
@@ -170,20 +174,21 @@ def min(alpha: int, beta: int):
 
                 # alpha-beta pruning
                 if minValue <= alpha:
-                    return (minValue, minPosition)
+                    return minValue, minPosition
                 if minValue < beta:
                     beta = minValue
 
-    return (minValue, minPosition)
+    return minValue, minPosition
 
 
-def main():
+def main() -> None:
+    print('Game begins!\nRows and columns are numerated from 1 to 3\n')
     clearBoard()
 
     global playerTurn
     playerTurn = 'X'
 
-    game()
+    playGame()
 
 
 if __name__ == "__main__":
