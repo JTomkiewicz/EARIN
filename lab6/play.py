d66ec7f8d5dc5d404670bc9c26dc0011d9716cf5
pkonrad1229 <pkonrad1229@gmail.com>
2022-06-09 21:07:31 +0200

    change play and train files

--- lab6/play.py
+++ lab6/play.py
@@ -1,17 +1,37 @@
 import gym
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DDPG, SAC, TD3, A2C
 import os
+import sys
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+args = sys.argv[1:]
+if (len(args) != 2):
+    print("Incorrect number of arguments! Please pass Name of the algorithm, and the name of the file")
+    exit()
+algorithm_name = args[0]
+if (algorithm_name == 'PPO'):
+    algorithm = PPO
+elif (algorithm_name == 'SAC'):
+    algorithm = SAC
+elif (algorithm_name == 'DDPG'):
+    algorithm = DDPG
+elif (algorithm_name == 'TD3'):
+    algorithm = TD3
+elif (algorithm_name == 'A2C'):
+    algorithm = A2C
+else:
+    print("Unsupported algorithm. Available algorithms: PPO, SAC, DDPG, TD3, A2C")
+    exit()
+
+model_path = os.path.join('training', args[1])
 
 env = gym.make('CarRacing-v1')
-env = DummyVecEnv([lambda: env])
 
-ppo_path = os.path.join('training', 'PPO_Cnn_100k')
-model = PPO.load(ppo_path, env=env)
+model = algorithm.load(model_path, env=env)
 
 obs = env.reset()
 
-dones = False
+done = False
 score = 0
 
 while True:
