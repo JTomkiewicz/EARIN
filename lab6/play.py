459c559b160f5b150eb1c9a7988d35998e55c299
pkonrad1229 <pkonrad1229@gmail.com>
2022-06-10 08:12:17 +0200

    format code and add notebook for model comparison

--- lab6/play.py
+++ lab6/play.py
@@ -2,30 +2,32 @@ import gym
 from stable_baselines3 import PPO, DDPG, SAC, TD3, A2C
 import os
 import sys
+
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 args = sys.argv[1:]
-if (len(args) != 2):
-    print("Incorrect number of arguments! Please pass Name of the algorithm, and the name of the file")
+if len(args) != 2:
+    print(
+        "Incorrect number of arguments! Please pass Name of the algorithm, and the name of the file"
+    )
     exit()
 algorithm_name = args[0]
-if (algorithm_name == 'PPO'):
+if algorithm_name == "PPO":
     algorithm = PPO
-elif (algorithm_name == 'SAC'):
+elif algorithm_name == "SAC":
     algorithm = SAC
-elif (algorithm_name == 'DDPG'):
+elif algorithm_name == "DDPG":
     algorithm = DDPG
-elif (algorithm_name == 'TD3'):
+elif algorithm_name == "TD3":
     algorithm = TD3
-elif (algorithm_name == 'A2C'):
+elif algorithm_name == "A2C":
     algorithm = A2C
 else:
     print("Unsupported algorithm. Available algorithms: PPO, SAC, DDPG, TD3, A2C")
     exit()
+model_path = os.path.join("training", args[1])
 
-model_path = os.path.join('training', args[1])
-
-env = gym.make('CarRacing-v1')
+env = gym.make("CarRacing-v1")
 
 model = algorithm.load(model_path, env=env)
 
@@ -41,5 +43,5 @@ while True:
     score += rewards
     if done:
         env.close()
-        print(f'Score: {score}')
+        print(f"Score: {score}")
         break
--- lab6/train.py
+++ lab6/train.py
@@ -3,35 +3,38 @@ from stable_baselines3 import PPO, DDPG, SAC, TD3, A2C
 import os
 import time
 import sys
+
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 args = sys.argv[1:]
-if (len(args) != 3 and len(args) != 4):
+if len(args) != 3 and len(args) != 4:
     print(len(args))
     print(args)
-    print("Incorrect number of arguments! Please pass Name of the algorithm, number of action steps of training, name of the output file and optionaly name of the model you wish to continue training")
+    print(
+        "Incorrect number of arguments! Please pass Name of the algorithm, number of action steps of training, name of the output file and optionaly name of the model you wish to continue training"
+    )
     exit()
 algorithm_name = args[0]
-if (algorithm_name == 'PPO'):
+if algorithm_name == "PPO":
     algorithm = PPO
-elif (algorithm_name == 'SAC'):
+elif algorithm_name == "SAC":
     algorithm = SAC
-elif (algorithm_name == 'DDPG'):
+elif algorithm_name == "DDPG":
     algorithm = DDPG
-elif (algorithm_name == 'TD3'):
+elif algorithm_name == "TD3":
     algorithm = TD3
-elif (algorithm_name == 'A2C'):
+elif algorithm_name == "A2C":
     algorithm = A2C
 else:
     print("Unsupported algorithm. Available algorithms: PPO, SAC, DDPG, TD3, A2C")
     exit()
 
-env = gym.make('CarRacing-v1')
+env = gym.make("CarRacing-v1")
 
 timesteps = int(args[1])
-write_path = os.path.join('training', args[2])
-if(len(args) == 4):
-    read_path = os.path.join('training', args[3])
+write_path = os.path.join("training", args[2])
+if len(args) == 4:
+    read_path = os.path.join("training", args[3])
     model = algorithm.load(read_path, env=env)
 else:
     model = algorithm("CnnPolicy", env, verbose=1)
@@ -41,5 +44,5 @@ start_time = time.time()
 
 model.learn(total_timesteps=timesteps, reset_num_timesteps=False)
 end_time = time.time()
-print(f'Training time: {end_time - start_time}')
+print(f"Training time: {end_time - start_time}")
 model.save(write_path)
