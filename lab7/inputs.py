d17431ea1de56c174f87420664ccf28b7264bff7
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-06-13 17:50:15 +0200

    init reading params

--- lab7/inputs.py
+++ lab7/inputs.py
@@ -1,13 +1,22 @@
-def readInt():
+def read_int():
     while True:
         try:
             number = input()
             number = int(number)
             break
         except ValueError:
-            print('Input must be a int!')
+            print('Input must be a integer!')
     return int(number)
 
 
 def read_params():
-    pass
+    print('STEP 1: Enter the evidence:')
+    evidence = input()
+
+    print('STEP 2: Enter the query:')
+    query = input()
+
+    print('STEP 3: Enter the number of steps:')
+    nr_steps = read_int()
+
+    return evidence, query, nr_steps
--- lab7/main.py
+++ lab7/main.py
@@ -4,12 +4,12 @@ from inputs import read_params
 
 def main():
     # read program parameters
-    # evidence, query, nr_steps = read_params()
-    # create a Bayesian network
+    evidence, query, nr_steps = read_params()
+    # create and print a Bayesian network
     bn = BayesianNetwork('flu.json')
     bn.print_network()
     # run the MCM algorithm
-    # bn.mcm(evidence, query, nr_steps)
+    bn.mcm(evidence, query, nr_steps)
 
 
 if __name__ == "__main__":
