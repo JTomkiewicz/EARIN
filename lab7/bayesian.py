8be48419ec01ede61b93a981f8e22f9e2f82503c
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-06-13 17:39:40 +0200

    do not store filename

--- lab7/bayesian.py
+++ lab7/bayesian.py
@@ -11,25 +11,25 @@ class Node:
 
 class BayesianNetwork:
     def __init__(self, file_name):
-        self.src_file = file_name
         self.nodes = []
-        self.read_json()
+        self.read_json(file_name)
 
     # read bayessian network from json file
-    def read_json(self):
-        with open(self.src_file, 'r') as f:
+    def read_json(self, file_name):
+        with open(file_name, 'r') as f:
             network = json.load(f)
 
         # check if json file is valid
         if network.get('nodes') is None or network.get('relations') is None:
             raise Exception('Invalid json file!')
 
-        # get keys
+        # crete nodes
         for key in network['nodes']:
             node = Node(key, network['relations'][key]['parents'],
                         network['relations'][key]['probabilities'])
             self.nodes.append(node)
 
+    # print bayesian network
     def print_network(self):
         print('\n--- Bayesian network: ---\n')
         for node in self.nodes:
@@ -37,5 +37,6 @@ class BayesianNetwork:
                 f'Name: {node.name}\nParents: {node.parents}\nProb: {node.probabilities}\n')
         print('--------------------------\n')
 
+    # run MCM algorithm
     def mcm(self, evidence, query, nr_steps):
         pass
